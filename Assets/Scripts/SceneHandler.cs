d38a9eceb7d393231e1fc3a364305475778c21e6
slow-cosmos <inki1030@naver.com>
2022-12-01 20:26:47 +0900

    게임 종료 버튼

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -8,14 +8,19 @@ public class SceneHandler : MonoBehaviour
     public void StartButton()
     {
         SceneManager.LoadScene("Play");
-        SoundManager.Instance.SfxPlay(0,0.2f);
+        SoundManager.Instance.SfxPlay(0, 0.2f);
         SoundManager.Instance.BgmPlay();
     }
 
     public void LobbyButton()
     {
         SceneManager.LoadScene("Lobby");
-        SoundManager.Instance.SfxPlay(0,0.2f);
+        SoundManager.Instance.SfxPlay(0, 0.2f);
         SoundManager.Instance.BgmStop();
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 }
